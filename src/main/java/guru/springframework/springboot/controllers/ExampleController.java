9a73425b63ed615da66dcdafeeae3518fdf3bb4e
Alex <azakotyansky@gmail.com>
2022-07-19 19:45:02 -0400

    some comments

--- src/main/java/guru/springframework/springboot/controllers/ExampleController.java
+++ src/main/java/guru/springframework/springboot/controllers/ExampleController.java
@@ -7,7 +7,19 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Created by jt on 2018-12-18.
+ http://localhost:8080
+ http://localhost:8080/actuator
+
+ When run mvn, before check and set
+ JAVA_HOME='C:\Program Files\Java\jdk-11.0.9'
+ echo $JAVA_HOME
+
+ mvn dependency:tree
+
+ run spring boot app from maven
+ mvn spring-boot:run
+ mvn clean package spring-boot:run
+
  */
 @RestController
 public class ExampleController {
