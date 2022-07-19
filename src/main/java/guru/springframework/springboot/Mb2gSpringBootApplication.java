1cafcf1e447d13b4aab6bd8a583d091d407b6633
Alex <azakotyansky@gmail.com>
2022-07-19 19:44:19 -0400

    some comments

--- src/main/java/guru/springframework/springboot/Mb2gSpringBootApplication.java
+++ src/main/java/guru/springframework/springboot/Mb2gSpringBootApplication.java
@@ -3,6 +3,21 @@ package guru.springframework.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ http://localhost:8080
+ http://localhost:8080/actuator
+
+ When run mvn, before check and set
+ JAVA_HOME='C:\Program Files\Java\jdk-11.0.9'
+ echo $JAVA_HOME
+
+ mvn dependency:tree
+
+ run spring boot app from maven
+ mvn spring-boot:run
+ mvn clean package spring-boot:run
+
+ */
 @SpringBootApplication
 public class Mb2gSpringBootApplication {
 
